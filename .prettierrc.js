dc42fa04c449a8db5180238e5efac8353965b45a
qiansw <1654563140@qq.com>
2019-09-25 20:32:01 +0800

    first package

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+    trailingComma: 'none',
+    arrowParens: 'always',
+    jsxBracketSameLine: true,
+    bracketSpacing: true
+};
